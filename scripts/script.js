6b3f1f667d893250ff29bba0545f0260a781de4a
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-05-15 18:22:36 -0700

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+let getArr = document.getElementById("getArr"),
+    pushArr = document.getElementById("pushArr"),
+    unshiftArr = document.getElementById("unshiftArr"),
+    shiftArr = document.getElementById("shiftArr"),
+    splitString = document.getElementById("splitString"),
+    spliceArr = document.getElementById("spliceArr"),
+    sortArr = document.getElementById("sortArr"),
+    nameToSearch = document.getElementById("nameToSearch"),
+    nameToSearch1 = document.getElementById("nameToSearch1"),
+    nameToSearch2 = document.getElementById("nameToSearch2"),
+    filterArr = document.getElementById("filterArr"),
+    mapArr = document.getElementById("mapArr"),
+    popArr = document.getElementById("popArr"),
+    joinArr = document.getElementById("joinArr"),
+    arrToString = document.getElementById("arrToString"),
+    arr = [],
+    studentToFind = "";
+
+getArr.addEventListener("click", function () {
+    console.log(arr);
+})
+
+pushArr.addEventListener("click", function () {
+    
+})
+
+unshiftArr.addEventListener("click", function () {
+    
+})
+
+shiftArr.addEventListener("click", function () {
+    
+})
+
+splitString.addEventListener("click", function () {
+   
+})
+
+spliceArr.addEventListener("click", function () {
+    
+})
+
+sortArr.addEventListener("click", function () {
+    
+})
+
+nameToSearch.addEventListener("keypress", function(event){
+   
+})
+
+filterArr.addEventListener("click", function(){
+   
+})
+
+mapArr.addEventListener("click", function(){
+    
+})
+
+popArr.addEventListener("click", function(){
+    
+})
+joinArr.addEventListener("click", function(){
+   
+})
+arrToString.addEventListener("click", function(){
+    
+})
